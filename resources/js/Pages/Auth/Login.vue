b86026384ed57a676d883b51f5b420e198a26673
root <root@JAMESULIP.localdomain>
2022-10-28 00:56:16 +0800

    changed login page to quasar

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,89 +1,79 @@
 <script setup>
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
-import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import Checkbox from '@/Components/Checkbox.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
+import AuthenticationCard from "@/Components/AuthenticationCard.vue";
+import AuthenticationCardLogo from "@/Components/AuthenticationCardLogo.vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
 
 defineProps({
-    canResetPassword: Boolean,
-    status: String,
+  canResetPassword: Boolean,
+  status: String,
 });
 
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
+  email: "",
+  password: "",
+  remember: false,
 });
 
 const submit = () => {
-    form.transform(data => ({
-        ...data,
-        remember: form.remember ? 'on' : '',
-    })).post(route('login'), {
-        onFinish: () => form.reset('password'),
+  form
+    .transform((data) => ({
+      ...data,
+      remember: form.remember ? "on" : "",
+    }))
+    .post(route("login"), {
+      onFinish: () => form.reset("password"),
     });
 };
 </script>
 
 <template>
-    <Head title="Log in" />
-
-    <AuthenticationCard>
-        <template #logo>
-            <AuthenticationCardLogo />
-        </template>
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+  <q-form @submit="submit()"
+    class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+  >
+    <q-card class="mx-auto w-96">
+      <q-card-section class="flex">
+        <div class="my-4 mx-auto">
+          <AuthenticationCardLogo />
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </AuthenticationCard>
+      </q-card-section>
+      <q-card-section>
+        <div class="q-gutter-md w-full">
+          <q-input
+            class="w-full"
+            :error="Boolean(form.errors.email)"
+            :error-message="form.errors.email"
+            type="text"
+            label="Email Address"
+            v-model="form.email"
+            outlined
+            dense
+          />
+          <q-input
+            class="w-full"
+            :error="Boolean(form.errors.password)"
+            :error-message="form.errors.password"
+            type="password"
+            label="Password"
+            v-model="form.password"
+            outlined
+            dense
+          />
+          <q-checkbox label="Remember me" dense v-model="form.remember" />
+        </div>
+      </q-card-section>
+      <q-separator />
+      <q-card-actions>
+        <a size="sm" v-if="canResetPassword" :href="route('password.request')">
+          Forgot your password?
+        </a>
+        <q-space />
+        <q-btn type="submit" unelevated color="primary" :loading="form.processing">Login</q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-form>
 </template>
